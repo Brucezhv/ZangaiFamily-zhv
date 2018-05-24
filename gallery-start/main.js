b7c44288bdc6f7b4a3fe181e3062ae6621dd2e07
gfd178 <gongfudou@gfd178deMacBook-Air.local>
2018-05-24 14:49:09 +0800

    laster demo

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -17,5 +17,16 @@ function displayImage(imgSrc) {
   displayedImage.setAttribute('src', imgSrc);
 }
 
-
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class'); 
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
 
