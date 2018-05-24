2a67a0a3ebff4bb586216ae0038ab249f7cbf2b7
gfd178 <gongfudou@gfd178deMacBook-Air.local>
2018-05-24 11:19:10 +0800

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   gallery-start/main.js

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -4,11 +4,9 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-
+for(var i=1;i<=5;i++){
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src','images/pic'+i+'.jpg');
   thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+}
 
